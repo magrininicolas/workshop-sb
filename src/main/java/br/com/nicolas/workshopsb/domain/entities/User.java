9058b823a2477be7bcf3f1fe81a602d3ab0b79b5
Nicolas <magrininicolas@outlook.com>
2023-08-11 12:17:37 -0300

    Add h2 database, JPA configuration for User entity

--- src/main/java/br/com/nicolas/workshopsb/domain/entities/User.java
+++ src/main/java/br/com/nicolas/workshopsb/domain/entities/User.java
@@ -3,12 +3,20 @@ package br.com.nicolas.workshopsb.domain.entities;
 import java.io.Serializable;
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity(name = "tb_users")
+@Table(name = "tb_users")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -18,14 +26,20 @@ public class User implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private UUID id;
 
+  @Column(nullable = false)
   private String name;
 
+  @Column(nullable = false, unique = true)
   private String email;
 
+  @Column(nullable = false, unique = true)
   private String phone;
 
+  @Column(nullable = false)
   private String password;
 
 }
