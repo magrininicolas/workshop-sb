92ada2143a647ebd0c77757d7a66c6eb2868304b
Nicolas <magrininicolas@outlook.com>
2023-08-11 10:55:09 -0300

    Project created

---
+++ src/main/java/br/com/nicolas/workshopsb/WorkshopSbApplication.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.workshopsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nicolas/workshopsb/WorkshopSbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.workshopsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
